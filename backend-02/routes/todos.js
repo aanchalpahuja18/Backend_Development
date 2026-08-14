e67296a540d005c7e34dfa561498ecf61988cf05
aanchalpahuja18 <aanchalpahuja34@gmail.com>
2026-08-14 15:13:34 +0530

    router file added

--- backend-02/routes/todos.js
+++ backend-02/routes/todos.js
@@ -5,9 +5,21 @@ const router = express.Router()
 
 //import controller
 const createTodo = require('../controllers/createTodo');
+const getTodo = require("../controllers/getTodo");
+const getTodoById = require("../controllers/getTodo");
 
+const updateTodo = require("../controllers/updateTodo");
+
+const deleteTodo = require("../controllers/deleteTodo");
 //define API route: here we have mapped the path with the controller
 router.post("/createTodo", createTodo);
 
+router.get("/getTodos", getTodo);
+
+router.get("/getTodos/:id", getTodoById);
+
+router.put("/updateTodo/:id", updateTodo);
+
+router.delete("/deleteTodo/:id", deleteTodo);
 
 module.exports = router;
