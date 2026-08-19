ad4168e5a1a6ec6891bd07663f35320f0b4a1b54
aanchalpahuja18 <aanchalpahuja34@gmail.com>
2026-08-19 21:16:36 +0530

    Router file added

--- backend-blogs-app/routes/blogRoutes.js
+++ backend-blogs-app/routes/blogRoutes.js
@@ -2,12 +2,17 @@ const express = require("express");
 
 const router = express.Router();
 
+//import controllers:
+const createComment = require("../controllers/comments");
+const {createPost, getPost} = require("../controllers/posts");
+const {likePost, unlikePost} = require("../controllers/likes");
 
-const createPost = require("../controllers/posts");
-const getPosts = require("../controllers/posts");
-
+//create mappings between routes and controllers:
+router.post("/comments/create", createComment)
 router.post("/posts/create", createPost);
-router.get("/posts", getPosts);
+router.get("/posts", getPost);
+router.post("/likes/like", likePost);
+router.post("/likes/unlike", unlikePost);
 
-
+//export
 module.exports = router;
