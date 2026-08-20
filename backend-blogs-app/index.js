4d0e78f6e2bf0b04257be9171f26863abe0e44a6
aanchalpahuja18 <aanchalpahuja34@gmail.com>
2026-08-20 21:27:28 +0530

    Comments added

--- backend-blogs-app/index.js
+++ backend-blogs-app/index.js
@@ -1,12 +1,15 @@
+//importing the instance of express framework
 const express = require("express");
 
+//building a working server with the help of express framwework
 const app = express();
 
+//with this we are loading the configurations of dotenv into our process object
 require("dotenv").config();
 
 const PORT = process.env.PORT || 8000;
 
-//middleware: to parse the json body;
+//middleware: to parse the json request body;
 app.use(express.json());
 
 const blogRoutes = require("./routes/blogRoutes");
