c198398834593d11cfcc508600cebdaf778178f5
aanchalpahuja18 <aanchalpahuja34@gmail.com>
2026-09-01 19:49:15 +0530

    Added login and protected routes

--- backend-auth-01/routes/user.js
+++ backend-auth-01/routes/user.js
@@ -2,10 +2,26 @@ const express = require("express");
 
 const router = express.Router();
 
-const signup = require("../controllers/auth");
+const {signup, login} = require("../controllers/auth");
+const {auth, isStudent, isAdmin} = require("../middlewares/auth");
 
 
-// router.post("/login", login);
+router.post("/login", login);
 router.post("/signup", signup);
+
+//protected routes:
+router.get("/student", auth, isStudent, (req, res) => {
+    res.json({
+        success: true,
+        message: "Welcome to the student portal"
+    })
+})
+
+router.get("/admin", auth, isAdmin, (req, res) => {
+    res.json({
+        success: true,
+        message: "Welcome to the student portal"
+    })
+})
 
 module.exports = router;
