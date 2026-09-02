0c5c932cd2841b559a0b7a93a53fa72279d561b3
aanchalpahuja18 <aanchalpahuja34@gmail.com>
2026-09-02 19:40:36 +0530

    Middleware function completed

--- backend-auth-01/middlewares/auth.js
+++ backend-auth-01/middlewares/auth.js
@@ -4,6 +4,78 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
-function auth(req, res){
+function auth(req, res, next){
+    try{
+        const token = req.body.token;
 
+        if(!token){
+            return res.status(401).json({
+                success: false,
+                message: "Token is missing!"
+            })
+        }
+
+        //verify the token:
+        try{
+            const decode = jwt.verify(token, process.env.JWT_SECRET);
+            console.log("printing decoded token:");
+            console.log(decode);
+
+            req.body = decode;
+            console.log("printing req body");
+            console.log(req.body);
+        }catch(err){
+            return res.status(401).json({
+                success: false,
+                message: "Token is invalid"
+            })
+        }
+
+           next();
+    } catch(err){
+        return res.status(500).json({
+            success: false,
+            message: "Token could not be verified!"
+        })
+    }
 }
+
+function isStudent(req, res, next){
+    try{
+        if(req.body.role !== "Student"){
+            return res.status(401).json({
+                success: false,
+                message: "This is a protected route only for students"
+            })
+        }
+
+        next();
+    }catch(err){
+        return res.status(401).json({
+            success: false,
+            message: "You do not have valid permission to enter here!"
+        })
+    }
+
+    next();
+}
+
+function isAdmin(req, res, next) {
+    try{
+        if(req.body.role !== "Admin"){
+            return res.status(401).json({
+                success: false,
+                message: "This is a protected route only for admins"
+            })
+        }
+
+        next();
+    } catch(err){
+        return res.status(401).json({
+            success: false,
+            message: "You do not have valid permission to enter here!"
+        })
+    }
+}
+
+module.exports = {auth, isStudent, isAdmin}
