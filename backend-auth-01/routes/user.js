7a4e381e389ffe6e7dd64c39025a012717feae62
aanchalpahuja18 <aanchalpahuja34@gmail.com>
2026-09-02 19:41:13 +0530

    Protected test route added

--- backend-auth-01/routes/user.js
+++ backend-auth-01/routes/user.js
@@ -9,6 +9,14 @@ const {auth, isStudent, isAdmin} = require("../middlewares/auth");
 router.post("/login", login);
 router.post("/signup", signup);
 
+//protected test route:
+router.get("/test", auth, (req, res) => {
+    res.json({
+        success: true,
+        message: "Welcome to the protected test route!"
+    })
+})
+
 //protected routes:
 router.get("/student", auth, isStudent, (req, res) => {
     res.json({
@@ -20,7 +28,7 @@ router.get("/student", auth, isStudent, (req, res) => {
 router.get("/admin", auth, isAdmin, (req, res) => {
     res.json({
         success: true,
-        message: "Welcome to the student portal"
+        message: "Welcome to the admin portal"
     })
 })
 
