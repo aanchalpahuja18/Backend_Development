a6ced3346734751b42a86fb0eed4b9f814e2ac58
aanchalpahuja18 <aanchalpahuja34@gmail.com>
2026-09-09 14:51:49 +0530

    Local File upload route created

--- backend-file-upload/routes/FileUpload.js
+++ backend-file-upload/routes/FileUpload.js
@@ -1,5 +1,14 @@
 //router 
-const router = require("express").Router();
+const express = require("express");
+const router = express.Router();
+
+const localFileUpload = require("../controllers/fileUpload");
+
+//api route
+// router.post("/imageUpload", imageUpload);
+// router.post("/videoUpload", videoUpload);
+// router.post("/imageReduceUpload", imageReduceUpload);
+router.post("/localFileUpload", localFileUpload)
 
 
 module.exports = router;
