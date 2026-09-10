b2beb2915a23637e84b39dd62c7685a6210877b1
aanchalpahuja18 <aanchalpahuja34@gmail.com>
2026-09-10 21:30:49 +0530

    Image reduce route added

--- backend-file-upload/routes/FileUpload.js
+++ backend-file-upload/routes/FileUpload.js
@@ -2,12 +2,12 @@
 const express = require("express");
 const router = express.Router();
 
-const {localFileUpload, imageUpload, videoUpload} = require("../controllers/fileUpload");
+const {localFileUpload, imageUpload, videoUpload, imageReduceUpload} = require("../controllers/fileUpload");
 
 //api route
 router.post("/imageUpload", imageUpload);
 router.post("/videoUpload", videoUpload);
-// router.post("/imageReduceUpload", imageReduceUpload);
+router.post("/imageReduceUpload", imageReduceUpload);
 router.post("/localFileUpload", localFileUpload)
 
 
