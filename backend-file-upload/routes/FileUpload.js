3951e61109f5554c864481a4e87e5c2a88a071f6
aanchalpahuja18 <aanchalpahuja34@gmail.com>
2026-09-10 15:02:51 +0530

    image upload route created

--- backend-file-upload/routes/FileUpload.js
+++ backend-file-upload/routes/FileUpload.js
@@ -2,10 +2,10 @@
 const express = require("express");
 const router = express.Router();
 
-const localFileUpload = require("../controllers/fileUpload");
+const {localFileUpload, imageUpload} = require("../controllers/fileUpload");
 
 //api route
-// router.post("/imageUpload", imageUpload);
+router.post("/imageUpload", imageUpload);
 // router.post("/videoUpload", videoUpload);
 // router.post("/imageReduceUpload", imageReduceUpload);
 router.post("/localFileUpload", localFileUpload)
