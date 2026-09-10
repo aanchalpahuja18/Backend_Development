51b0b1fe086f0f595deaec0fce6c4bc97a0de970
aanchalpahuja18 <aanchalpahuja34@gmail.com>
2026-09-10 15:02:12 +0530

    image Upload handler function added

--- backend-file-upload/controllers/fileUpload.js
+++ backend-file-upload/controllers/fileUpload.js
@@ -1,4 +1,5 @@
 const File = require("../models/File");
+const cloudinary = require("cloudinary").v2;
 
 //localFileUpload -> handler function:
  
@@ -33,4 +34,64 @@ async function localFileUpload(req, res) {
     }
 }
 
-module.exports = localFileUpload;
+//image upload:
+
+function checkFileTypeSupported(type, supportedTypes){
+    return supportedTypes.includes(type);
+}
+
+async function uploadFileToCloudinary(file, folder){
+    console.log("here3")
+    const options = {folder};
+    return await cloudinary.uploader.upload(file.tempFilePath, options);
+}
+
+async function imageUpload(req, res) {
+    try{
+        //data fetch
+        const {name, email, tags} = req.body;
+        console.log(name, email, tags);
+        const file = req.files.imageFile;
+        console.log(file);
+
+        console.log("here");
+
+        //validation
+        const supportedTypes = ["jpg", "jpeg", "png"];
+        if(!name || !email || !tags){
+            return res.status(500).json({
+                success: false,
+                message: "Please enter all the required fields"
+            })
+        }
+        const fileType = file.name.split(".")[1].toLowerCase();
+
+        if(!checkFileTypeSupported(fileType, supportedTypes)){
+            return res.status(400).json({
+                success: false,
+                message: "File format is not supported in our system!"
+            })
+        }
+        console.log("her2")
+
+        //file format supported:
+        const response = await uploadFileToCloudinary(file, "Aanchal_Files");
+        console.log("here4")
+        console.log(response);
+
+        return res.status(200).json({
+            success: true,
+            message: "Image uploaded successfully!"
+        })
+        
+    }
+    catch(err){ 
+        console.log(err);
+        return res.status(400).json({
+            success: false,
+            message: "Something went wrong!"
+        })
+    }
+}
+
+module.exports = {localFileUpload, imageUpload};
