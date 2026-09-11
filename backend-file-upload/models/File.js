c7c57e78b2012e2d45a7f948ba15694d47704255
aanchalpahuja18 <aanchalpahuja34@gmail.com>
2026-09-11 15:19:12 +0530

    Mailing functionality added

--- backend-file-upload/models/File.js
+++ backend-file-upload/models/File.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
+const nodemailer = require("nodemailer");
+
+require("dotenv").config();
 
 const fileSchema = mongoose.Schema({
     name: {
@@ -15,6 +18,33 @@ const fileSchema = mongoose.Schema({
     },
     url: {
         type: String
+    }
+})
+
+fileSchema.post("save", async function (doc) {
+    try{
+        console.log("Document: ", doc);
+
+        //transporter
+        //TODO: shift this configuration under /config folder:
+        const transporter = nodemailer.createTransport({
+            host: process.env.MAIL_HOST,
+            auth: {
+                user: process.env.MAIL_USER,
+                pass: process.env.MAIL_PASSWORD
+            }
+        })
+
+        //send mail
+        let info = await transporter.sendMail({
+            from: "Aanchal Pahuja",
+            to: doc.email,
+            subject: "New File Uploaded on Cloudinary",
+            html: `<h2>Hello World</h2> <p>File Uploaded Successfully</p>`
+        })
+        console.log("Info object", info)
+    } catch(err){
+        console.log("Error: ", err);
     }
 })
 
